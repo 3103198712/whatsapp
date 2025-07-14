f830a4c612869e20365749c224059e336b5b9702
3103198712 <jeffersonv.nascimento@gmail.com>
2025-07-14 17:59:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
-// whatsapp
-
-// Importar Express.js const express = require ( 'express' );
-
-
-// Crie um aplicativo Express const app = express ();
-
-
-// Middleware para analisar corpos JSON 
-app . use ( express . json ());
-
-// Definir porta e verify_token const port = process . env . PORT || 3000 ; const verifyToken = process . env . VERIFY_TOKEN ;
- 
-
-
-// Rota para solicitações GET 
-app . get ( '/' , ( req , res ) => { const { 'hub.mode' : mode , 'hub.challenge' : challenge , 'hub.verify_token' : token } = req . query ;   
-       
-
-  if ( modo === 'inscrever-se' && token === verifyToken ) { 
-    console . log ( 'WEBHOOK VERIFICADO' ); 
+/opt/render/project/src/app.js:22
     res . status ( 200 ). enviar ( desafio ); } senão { 
-    res . status ( 403 ). fim (); } });    
-    
-  
-
-
-// Rota para solicitações POST 
-app . post ( '/' , ( req , res ) => { const timestamp = new Date (). toISOString (). replace ( 'T' , ' ' ). slice ( 0 , 19 ); 
-  console . log (` \n\n Webhook recebido $ { timestamp } \n `); 
-  console . log ( JSON . stringify ( req . body , null , 2 )); 
-  res . status ( 200 ). end (); });   
-        
-
-
-// Inicie o aplicativo do servidor . 
-listen ( port , ( ) = > { 
-  console.log (` \n Escutando na porta $ { port } \n` ); });   
+                                                      ^
+SyntaxError: Unexpected token '{'
+    at wrapSafe (node:internal/modules/cjs/loader:1662:18)
+    at Module._compile (node:internal/modules/cjs/loader:1704:20)
+    at Object..js (node:internal/modules/cjs/loader:1895:10)
+    at Module.load (node:internal/modules/cjs/loader:1465:32)
+    at Function._load (node:internal/modules/cjs/loader:1282:12)
+    at TracingChannel.traceSync (node:diagnostics_channel:322:14)
+    at wrapModuleLoad (node:internal/modules/cjs/loader:235:24)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:171:5)
+    at node:internal/main/run_main_module:36:49
+Node.js v22.16.0
