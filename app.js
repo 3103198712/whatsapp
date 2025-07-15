b6ad41dbd414defc39e2aabcc9dac10bf9d2b05b
3103198712 <jeffersonv.nascimento@gmail.com>
2025-07-15 14:59:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
-
+  {{ $json.query['hub.challenge']}}
   if (mode === 'subscribe' && token === verifyToken) {
     res.status(200).send(challenge);
   } else {
